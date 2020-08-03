16f475a415fb9d620024451ad90f9451337e845c
LeonLin0516 <52585348+LeonLin0516@users.noreply.github.com>
2020-08-03 16:50:37 -0700

    completed ex5

--- ex5/linearRegCostFunction.m
+++ ex5/linearRegCostFunction.m
@@ -19,16 +19,18 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+% cost function
+cost = sum((X * theta - y) .^2) / (2 * m);
+cost_regularization = theta .^ 2;
+cost_regularization(1) = 0;
+cost_regularization = sum(cost_regularization);
+J = cost + (lambda / (2 * m)) * cost_regularization;
 
-
-
-
-
-
-
-
-
-
+% gradient
+grad = (1 / m) * sum((X * theta - y) .* X);
+grad_regularization = (lambda / m) * theta;
+grad_regularization(1) = 0;
+grad = grad' + grad_regularization;
 
 % =========================================================================
 
